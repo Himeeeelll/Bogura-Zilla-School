9e8804883560542a0a6e48da75babf49b92d9e7d
Himel Das <himeeeelll3436@gmail.com>
2022-07-12 22:12:35 +0600

    update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
+                }, 1000);
+            }
+        } else {
+            document.querySelector(".text_hide").animate([
+                { left: `${left_instance}%` },
+                { left: `${left_instance + ((100 / text_len) * (word_len[i] + 1))}%` }
+            ], timings_2);
 
+            document.querySelector(".text_cursor").animate([
+                { left: `${left_instance}%` },
+                { left: `${left_instance + ((100 / text_len) * (word_len[i] + 1))}%` }
+            ], timings_2);
+        }
+
+        left_instance = left_instance + ((100 / text_len) * (word_len[i] + 1));
+    }
+}
+typing_animation();
